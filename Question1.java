62ebf4faa66288255c2ed6a56f44cbec77ee8562
kongda <djkong11@gmail.com>
2015-12-10 10:41:02 -0800

    some updates.

--- Question1.java
+++ Question1.java
@@ -8,21 +8,21 @@
 
 // Definition for singly-linked list.
 // class ListNode {
-//     int val;
-//     ListNode next;
-//     ListNode(int x) {
-//         val = x;
-//         next = null;
-//     }
+//	 int val;
+//	 ListNode next;
+//	 ListNode(int x) {
+//		 val = x;
+//		 next = null;
+//	 }
 // }
 
 public class Question1 {
 
-    // A straightforward solution would be iterating the list and using a HashSet
-    // to store unique nodes. When we encounter a node which is already in the set,
-    // it means this list has a cycle. 
-    // Suppose there are n nodes in the list, the time complexity is O(n), 
-    // the space complexity is O(n).    
+	// A straightforward solution would be iterating the list and using a HashSet
+	// to store unique nodes. When we encounter a node which is already in the set,
+	// it means this list has a cycle. 
+	// Suppose there are n nodes in the list, the time complexity is O(n), 
+	// the space complexity is O(n).	
 	public boolean hasCycle(ListNode head) {
 		HashSet<ListNode> set = new HashSet<ListNode>();
 		while (head != null){
@@ -34,28 +34,28 @@ public class Question1 {
 		return false;
 	}
 
-    // Another solution is to use two pointers. One of them moves one step each 
-    // time. The other moves two steps each time. Eventually they will reach the 
-    // same node. This could be demonstrated by going through several examples. 
-    // If there is indeed a cycle, the fast pointer will always be one loop 
-    // faster than the slow pointer when they reach the same node. So the time 
-    // complexity is O(n), the space complexity is O(1).
-    public boolean hasCycle1(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        
-        if (head != null && head.next == head) return true;
-        
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+	// Another solution is to use two pointers. One of them moves one step each 
+	// time. The other moves two steps each time. Eventually they will reach the 
+	// same node. This could be demonstrated by going through several examples. 
+	// If there is indeed a cycle, the fast pointer will always be one loop 
+	// faster than the slow pointer when they reach the same node. So the time 
+	// complexity is O(n), the space complexity is O(1).
+	public boolean hasCycle1(ListNode head) {
+		ListNode fast = head;
+		ListNode slow = head;
+		
+		if (head != null && head.next == head) return true;
+		
+		while(fast != null && fast.next != null){
+			slow = slow.next;
+			fast = fast.next.next;
  
-            if(slow == fast)
-                return true;
-        }
+			if(slow == fast)
+				return true;
+		}
  
-        return false;        
-    }    
+		return false;		
+	}	
 }
 
 
