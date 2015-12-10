55ad88d9937d2edfd7571aaf8230af620b7a704d
kongda <djkong11@gmail.com>
2015-12-10 10:39:50 -0800

    some updates.

--- Question1.java
+++ Question1.java
@@ -16,14 +16,13 @@
 //     }
 // }
 
-
-// A straight forward solution would be iterating the list and using a HashSet
-// to store unique nodes. When we encounter a node which is already in the set,
-// it means this list has a cycle. 
-// Suppose there are n nodes in the list, the time complexity is O(n), 
-// the space complexity is O(n).
-
 public class Question1 {
+
+    // A straightforward solution would be iterating the list and using a HashSet
+    // to store unique nodes. When we encounter a node which is already in the set,
+    // it means this list has a cycle. 
+    // Suppose there are n nodes in the list, the time complexity is O(n), 
+    // the space complexity is O(n).    
 	public boolean hasCycle(ListNode head) {
 		HashSet<ListNode> set = new HashSet<ListNode>();
 		while (head != null){
@@ -34,17 +33,14 @@ public class Question1 {
 		}
 		return false;
 	}
-}
 
-// Another solution is to use two pointers. One of them moves one step each 
-// time. The other moves two steps each time. Eventually they will reach the 
-// same node. This could be demonstrated by going through several examples. 
-// If there is indeed a cycle, the fast pointer will always be one loop 
-// faster than the slow pointer when they reach the same node. So the time 
-// complexity is O(n), the space complexity is O(1).
-
-public class Question1 {
-    public boolean hasCycle(ListNode head) {
+    // Another solution is to use two pointers. One of them moves one step each 
+    // time. The other moves two steps each time. Eventually they will reach the 
+    // same node. This could be demonstrated by going through several examples. 
+    // If there is indeed a cycle, the fast pointer will always be one loop 
+    // faster than the slow pointer when they reach the same node. So the time 
+    // complexity is O(n), the space complexity is O(1).
+    public boolean hasCycle1(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
         
@@ -59,5 +55,8 @@ public class Question1 {
         }
  
         return false;        
-    }
+    }    
 }
+
+
+
